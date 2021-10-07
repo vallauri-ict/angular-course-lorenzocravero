bc1693113523fde9dc5011079a52bb7e801bb59e
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-10-07 11:15:55 +0200

    Aggiunti e visualizzati i dati degli studenti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Corso angular';
+  title = '5B INF 2021/22';
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  studentList = [
+    { name: "Lorenzo Cravero", hobby: "Calcio" , gender: "M"},
+    { name: "Edoardo Abebei", hobby: "Coding" , gender: "M"},
+    { name: "Ivan Angjelovski", hobby: "Palestra" , gender: "M"},
+    { name: "Fabio Cavallero", hobby: "Calcio" , gender: "F"},
+    { name: "Gleni Ruci", hobby: "Coding" , gender: "M"},
+    { name: "Emanuele Altavlilla", hobby: "Calcio" , gender: "M"},
+    { name: "Giada Valinotto", hobby: "Cinema" , gender: "F"},
+    { name: "Michele Stefanachi", hobby: "Calcio" , gender: "M"},
+    { name: "Paolo Giordano", hobby: "Cinema" , gender: "M"},
+    { name: "Luca Pelissero", hobby: "Palestra" , gender: "M"},
+    { name: "Bianca Teleman", hobby: "Coding" , gender: "F"},
+    { name: "Gabriele Leone", hobby: "Palestra" , gender: "F"},
+  ]
+
+  student :any;
+  constructor() { 
+    //tutte le volte che verrà istanziato uno studente, uno di questi verrà preso a caso
+    let n = Math.floor(Math.random() * this.studentList.length);
+    this.student = this.studentList[n];
+  }
 
   ngOnInit(): void {
   }
