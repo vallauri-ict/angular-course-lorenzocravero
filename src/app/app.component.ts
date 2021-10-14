01d86d526002221293b1c17fc3b9ff5cbbae0845
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-10-14 11:14:24 +0200

    utilizzato ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '5B INF 2021/22';
+  studentList = [
+    { name: "Lorenzo Cravero", hobby: "Calcio" , gender: "M", isPro : false},
+    { name: "Edoardo Abebei", hobby: "Coding" , gender: "M", isPro : false},
+    { name: "Ivan Angjelovski", hobby: "Palestra" , gender: "M", isPro : false},
+    { name: "Fabio Cavallero", hobby: "Calcio" , gender: "F", isPro : false},
+    { name: "Gleni Ruci", hobby: "Coding" , gender: "M", isPro : false},
+    { name: "Emanuele Altavlilla", hobby: "Calcio" , gender: "M", isPro : false},
+    { name: "Giada Valinotto", hobby: "Cinema" , gender: "F", isPro : false},
+    { name: "Michele Stefanachi", hobby: "Calcio" , gender: "M", isPro : false},
+    { name: "Paolo Giordano", hobby: "Cinema" , gender: "M", isPro : false},
+    { name: "Luca Pelissero", hobby: "Palestra" , gender: "M", isPro : false},
+    { name: "Bianca Teleman", hobby: "Coding" , gender: "F", isPro : false},
+    { name: "Gabriele Leone", hobby: "Palestra" , gender: "F", isPro : false},
+  ]
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student',
@@ -6,38 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
+  @Input()student:any;
 
-  studentList = [
-    { name: "Lorenzo Cravero", hobby: "Calcio" , gender: "M", isPro : false},
-    { name: "Edoardo Abebei", hobby: "Coding" , gender: "M", isPro : false},
-    { name: "Ivan Angjelovski", hobby: "Palestra" , gender: "M", isPro : true},
-    { name: "Fabio Cavallero", hobby: "Calcio" , gender: "F", isPro : true},
-    { name: "Gleni Ruci", hobby: "Coding" , gender: "M", isPro : false},
-    { name: "Emanuele Altavlilla", hobby: "Calcio" , gender: "M", isPro : false},
-    { name: "Giada Valinotto", hobby: "Cinema" , gender: "F", isPro : true},
-    { name: "Michele Stefanachi", hobby: "Calcio" , gender: "M", isPro : false},
-    { name: "Paolo Giordano", hobby: "Cinema" , gender: "M", isPro : false},
-    { name: "Luca Pelissero", hobby: "Palestra" , gender: "M", isPro : true},
-    { name: "Bianca Teleman", hobby: "Coding" , gender: "F", isPro : true},
-    { name: "Gabriele Leone", hobby: "Palestra" , gender: "F", isPro : false},
-  ]
 
-  student :any;
   constructor() { 
-    this.randomStudent();
+    
   }
 
-  private randomStudent(){
+  private randomStudentPro(){
     //tutte le volte che verrà istanziato uno studente, uno di questi verrà preso a caso
-    let n = Math.floor(Math.random() * this.studentList.length);
-    this.student = this.studentList[n];
+    let n = Math.floor(Math.random() * 2);
+    if(n == 1) this.student.isPro = true;
   }
 
   onStudentClick(){
-    this.randomStudent();
+    this.student.isPro = !this.student.isPro;
   }
 
   ngOnInit(): void {
+    this.randomStudentPro();
   }
-
 }
